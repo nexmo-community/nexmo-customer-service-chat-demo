3a3f9b3234c31dc99376e57e93919f9580a5cc74
leggetter <phil@leggetter.co.uk>
2016-09-27 21:30:38 +0100

    Add skip_before_action. Support GET & POST
    * Fixes to align with tutorial
    * Still need to make a tutorial tweak

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,7 +1,19 @@
 class TextMessagesController < ApplicationController
   include ApplicationHelper
+  
+  skip_before_action :verify_authenticity_token, :only => [:create]
 
+  # If webhooks are set up as GET requests
   def index
+    create_message(params)
+  end
+ 
+  # If webhooks are set up as POST requests
+  def create
+    create_message(params)
+  end
+
+  def create_message(params)
     message = Message.create!(
       number: params[:msisdn],
       text: params[:text],
